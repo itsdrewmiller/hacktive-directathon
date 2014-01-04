c13cf086e119a41793c8240672192de5186fd2e8
Drew Miller <drew@drewmiller.net>
2014-01-03 22:28:33 -0500

    Adding random seed for client side access

--- hd.js
+++ hd.js
@@ -36,6 +36,7 @@ app.use(passport.initialize());
 app.use(passport.session());
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/lib', express.static(path.join(__dirname, 'lib')));
 
 
 passport.use(new FacebookStrategy({
